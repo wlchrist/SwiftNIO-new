9a0ea1b949f40452142572105c2e420e1cf3e161
Warren Christian <wchristian2000@gmail.com>
2024-04-16 13:19:46 -0700

    login boilerplate
    added a login feature

--- SwiftNIO/ContentView.swift
+++ SwiftNIO/ContentView.swift
@@ -9,17 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var netLogicSession = networkLogic()
+    @StateObject private var netLogicSession = networkLogic()
+    
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var loginStatus: String = ""
+    
     var body: some View {
+        
         VStack {
+            TextField("Username", text: $username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            TextField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
             Button("Connect to server") {
                 self.netLogicSession.setupConnection()
                 
             }
-                
-            .padding()
-            .background(Color.blue)
-            .cornerRadius(10)
+            Button("Login") {
+                netLogicSession.sendLogin(username: username, password: password)
+            }
+            
+            Text(netLogicSession.statusMessage)
+                .padding()
         }
         .padding()
     }
--- SwiftNIO/SwiftNIOApp.swift
+++ SwiftNIO/SwiftNIOApp.swift
@@ -10,31 +10,84 @@ import NIO
 
 // GOAL: Create a TCP socket connection to localhost ftp server. List each step required for a successful SwiftNIO connection.
 
-class networkLogic {
+class networkLogic: ObservableObject {
     let group: EventLoopGroup // Event loop groups, are a truncation of multiple event workers which wait for network operations.
+    @Published var statusMessage: String = ""
+    var channel: Channel?
+    
+    
     init() {
         // personal note: init can (in some cases) negate the need to declare and optional explicitly
         self.group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
     }
     
+    func sendString(_ command: String, channel: Channel) -> EventLoopFuture<Void> {
+        let cleanString = command + "\r\n" // NOTE: string sent over socket isn't "clean"/formatted correctly. denoting a new line and return, ensures the command is complete by the time you enter a new string to be send to the FTP server
+        
+        let buffer = channel.allocator.buffer(string: cleanString)
+        
+        return channel.writeAndFlush(buffer)
+        
+        
+    }
+    
     func setupConnection() {
         let bootstrap = ClientBootstrap(group: self.group)
-        print("1. bootstrap created")
-        
+            .channelInitializer { channel in
+                channel.pipeline.addHandlers([
+                   // responseDecoder(),
+                   // responseHandler(),
+                   // commandEncoder(),
+                   // commandSender()
+                    
+                ])
+            }
+
         let connectionAttempt = bootstrap.connect(host: "localhost", port: 21)
         
         connectionAttempt.whenSuccess { channel in
-            print("2. successful connection")
+            DispatchQueue.main.async {
+                self.statusMessage = "Connected Successfully"
+                self.channel = channel
+            }
         }
         
-        connectionAttempt.whenFailure { channel in
-            print("2. failed connection \(channel)")
+        connectionAttempt.whenFailure { error in
+            DispatchQueue.main.async {
+                self.statusMessage = "Failed Connection \(error.localizedDescription)"
+            }
         }
     }
     
-    func sendLogin() {
+    func sendLogin(username: String, password: String) {
+        guard let channel = self.channel else {
+            DispatchQueue.main.async {
+                self.statusMessage = "No active pipeline to send commands through"
+            }
+            return
+        }
         
+        sendString("USER \(username)", channel: channel)
+            .flatMap {
+                self.sendString("PASS \(password)", channel: channel)
+            }
+            .whenComplete { result in
+                DispatchQueue.main.async {
+                    switch result {
+                    case .success:
+                        self.statusMessage = "Login Successful"
+                    case .failure:
+                        self.statusMessage = "Login Failed: \(result)"
+                        
+                    }
+                }
+            }
     }
+    
+
+    
+    
+    
 }
 
 @main
